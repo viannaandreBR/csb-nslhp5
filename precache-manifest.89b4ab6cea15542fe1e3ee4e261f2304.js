e66c3d81581b51ddac6e7fdb607d5b28a81d55ee
Code Sandbox <hello@codesandbox.io>
2022-04-16 02:38:47 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.89b4ab6cea15542fe1e3ee4e261f2304.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22eeee888ca37ab76e53b60898356d6",
+    "url": "https://viannaandrebr.github.io/csb-nslhp5/index.html"
+  },
+  {
+    "revision": "c5cf25d19358a67699bb",
+    "url": "https://viannaandrebr.github.io/csb-nslhp5/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "c7a8a8cee54900a82382",
+    "url": "https://viannaandrebr.github.io/csb-nslhp5/static/js/2.68feb8b5.chunk.js"
+  },
+  {
+    "revision": "c5cf25d19358a67699bb",
+    "url": "https://viannaandrebr.github.io/csb-nslhp5/static/js/main.4625a669.chunk.js"
+  },
+  {
+    "revision": "54e6fbff92c6dd8e1580",
+    "url": "https://viannaandrebr.github.io/csb-nslhp5/static/js/runtime~main.556234c3.js"
+  }
+]);
